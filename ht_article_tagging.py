1d5341dc20cf21e3f2518a2fbbc96253a20dd0b2
Abhishek <abhishek.gupta@hindustantimes.com>
2017-06-18 13:58:57 +0530

    score keyphrases by tfidf

--- ht_article_tagging.py
+++ ht_article_tagging.py
@@ -1,4 +1,9 @@
 import sys
+import glob
+import errno
+import string
+import io
+from gensim import corpora, models, similarities
 
 def extract_candidate_chunks(text,grammer=r'KT: {(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}'):
     import itertools, nltk, string
@@ -10,7 +15,7 @@ def extract_candidate_chunks(text,grammer=r'KT: {(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN
 
     tagged_sents = nltk.pos_tag_sents(nltk.word_tokenize(sent) for sent in nltk.sent_tokenize(text))
 
-    all_chunks = list(itertools.chain.from_iterable(nltk.chunk.tree2conlltags(chunker.parse(tagged_sents))
+    all_chunks = list(itertools.chain.from_iterable(nltk.chunk.tree2conlltags(chunker.parse(tagged_sent))
                                                     for tagged_sent in tagged_sents))
 
 
@@ -38,33 +43,47 @@ def extract_candidate_words(text,good_tags=set(['JJ','JJR','JJS','NN','NNP','NNS
     return candidates
 
 
-def score_keyphrases_by_tfidf(texts,candidates='chunks'):
+def score_keyphrases_by_tfidf(texts, candidates='chunks'):
     import gensim, nltk
 
     # extract candidates from each text in texts, either chunks or words
-
     if candidates == 'chunks':
         boc_texts = [extract_candidate_chunks(text) for text in texts]
     elif candidates == 'words':
         boc_texts = [extract_candidate_words(text) for text in texts]
-
     # make gensim dictionary and corpus
-
     dictionary = gensim.corpora.Dictionary(boc_texts)
     corpus = [dictionary.doc2bow(boc_text) for boc_text in boc_texts]
-
     # transform corpus with tf*idf model
-
     tfidf = gensim.models.TfidfModel(corpus)
     corpus_tfidf = tfidf[corpus]
 
-    return corpus_tfidf,dictionary
+
+    return corpus_tfidf ,dictionary
+
 
 
 
 
 def main():
-    print('hello world')
+    texts = []
+    path = 'docs/*.txt'
+    files = glob.glob(path)
+
+    for name in files:
+        try:
+            with open(name) as f:
+                content = f.readline()
+                content = ''.join([c for c in content if c not in ("!", "?","'",".",",",":","`")])
+                texts.append(content)
+        except IOError as exc:  # Not sure what error this is
+            if exc.errno != errno.EISDIR:
+                raise
+
+
+    # print extract_candidate_chunks(texts[0])
+    print score_keyphrases_by_tfidf(texts)
+
 
 if __name__ == '__main__':
     main()
